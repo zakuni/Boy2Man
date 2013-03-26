0a2d43838b28584fa6dddc2c485a8f8c9024fac3
zakuni <kunio038@gmail.com>
2013-03-27 00:04:50 +0900

    Add test_Boy2Man
    add test_select_hand

---
+++ test/test_Boy2Man.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'test_helper'
+
+class TestBoy2Man < MiniTest::Unit::TestCase
+  include Boy2Man
+
+  def setup
+    @stand = Boy2Man.new
+  end
+
+  def test_select_hand
+    100.times do
+      assert_includes ["グー", "チョキ", "パー"], @stand.select_hand
+    end
+  end
+end
